e198e7a65633689a6baec054f99a1c8a2b23a224
jeremiahlee94 <jhwlee94@gmail.com>
2021-12-07 13:52:13 -0800

    make state variables, cached elements, and basic event listeners and functions

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 /*----- constants -----*/
-// class Disk {
+// class DISK {
 //     constructor(num) {
 //         this.num = num
 //     }
@@ -10,20 +10,23 @@
 let gameFinished;
 // variable number of disks
 let stackHeight;
-// variable to see if the div element is interacted with
-let moved;
+// selected disk
+let selectDisk;
 
 /*----- cached element references -----*/
 const aboutGameBtn = document.querySelector('.aboutGame');
 const startGame = document.querySelector('.initGame');
 const resetGameBtn = document.querySelector('.resetGame');
 const towerHeight = document.querySelector('.towerHeight');
+const towerContainer = document.querySelector('.gameContainer');
 const stackOne = document.querySelector('.stackOne');
 const stackTwo = document.querySelector('.stackTwo');
 const stackThree = document.querySelector('.stackThree');
 
 /*----- event listeners -----*/
 startGame.addEventListener('click', init);
+// resetGameBtn.addEventListener('click', render);
+towerContainer.addEventListener('click', select);
 
 /*----- functions -----*/
 // init function
@@ -31,40 +34,58 @@ startGame.addEventListener('click', init);
 function init() {
     // set win condition to false
     gameFinished = false;
-    moved = false;
+    startPos = false;
     stackHeight = parseInt(towerHeight.value);
     render();
+    track();
 }
 
 // render function
 function render() {
+    // clear current tower
+    // while(stackOne.contains(div)) {
+    //     stackOne.removeChild();
+    // }
+
     // create as many disks as the input value
     for (let i = 1; i < stackHeight + 1; i++) {
         // create div element
         const disk = document.createElement('div');
-        // add event listeners to the div element <- maybe add them to parent element
-        disk.addEventListener('mousedown', function() {
-            moved = false;
-            console.log('down', moved);
-        });
-        disk.addEventListener('mousemove', function() {
-            moved = true;
-            console.log('hold', moved);
-        });
-        disk.addEventListener('mouseup', function() {
-            if(moved === true) {
-                console.log('down and hold');
-            }
-        });
         // add class to div element
-        disk.classList.add(i);
+        disk.classList.add(i, 'disk');
         // add css to div element for testing purposes
         disk.style.paddingLeft = '25px';
         disk.style.paddingRight = '25px';
+        disk.style.border = 'solid red';
         // fill inner text of div element for testing purposes
         disk.innerText = 'testing' + i;
         // append the div element to the parent div element
         stackOne.appendChild(disk);
-        // console.log(disk.classList);
+    }
+}
+
+// https://stackoverflow.com/questions/24050738/javascript-how-to-dynamically-move-div-by-clicking-and-dragging
+function track(event) {
+    // towerContainer.addEventListener('mousedown', function() {
+    //     moved = false;
+    //     // console.log('down', moved);
+    // });
+    // towerContainer.addEventListener('mousemove', function() {
+    //     moved = true;
+    //     // console.log('hold', moved);
+    // });
+    // towerContainer.addEventListener('mouseup', function(event) {
+    //     if(moved === true) {
+    //         // console.log('down and hold');
+    //     }
+    //     console.log(event);
+    //     move(event);
+    // });
+}
+
+function select(event) {
+    if(event.target.classList.contains('disk')) {
+        selectDisk = event.target;
+        // console.log(selectDisk);
     }
 }
