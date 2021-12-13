6cf4e9a83d4516751f2906d1d1206a52e587ee5b
jeremiahlee94 <jhwlee94@gmail.com>
2021-12-13 13:08:29 -0800

    Add pseudocode for solution stretch goal

--- script.js
+++ script.js
@@ -8,6 +8,51 @@ class DISK {
     }
 }
 
+// Auto solution stretch goal
+    // Simpler statement of iterative solution
+        // For an even number of disks:
+
+        // make the legal move between pegs A and B (in either direction),
+        // make the legal move between pegs A and C (in either direction),
+        // make the legal move between pegs B and C (in either direction),
+        // repeat until complete.
+        // For an odd number of disks:
+
+        // make the legal move between pegs A and C (in either direction),
+        // make the legal move between pegs A and B (in either direction),
+        // make the legal move between pegs B and C (in either direction),
+        // repeat until complete.
+        // In each case, a total of 2n − 1 moves are made.
+
+        // Equivalent iterative solution
+        // Another way to generate the unique optimal iterative solution:
+
+        // Number the disks 1 through n (largest to smallest).
+
+        // If n is odd, the first move is from peg A to peg C.
+        // If n is even, the first move is from peg A to peg B.
+        // Now, add these constraints:
+
+        // No odd disk may be placed directly on an odd disk.
+        // No even disk may be placed directly on an even disk.
+        // There will sometimes be two possible pegs: one will have disks, and the other will be empty. Place the disk on the non-empty peg.
+        // Never move a disk twice in succession.
+
+    // Recursive solution
+        // Illustration of a recursive solution for the Towers of Hanoi puzzle with 4 disks
+        // The key to solving a problem recursively is to recognize that it can be broken down into a collection of smaller sub-problems, to each of which that same general solving procedure that we are seeking applies, and the total solution is then found in some simple way from those sub-problems' solutions. Each of these created sub-problems being "smaller" guarantees that the base case(s) will eventually be reached. Thence, for the Towers of Hanoi:
+
+        // label the pegs A, B, C,
+        // let n be the total number of disks,
+        // number the disks from 1 (smallest, topmost) to n (largest, bottom-most).
+        // Assuming all n disks are distributed in valid arrangements among the pegs; assuming there are m top disks on a source peg, and all the rest of the disks are larger than m, so they can be safely ignored; to move m disks from a source peg to a target peg using a spare peg, without violating the rules:
+
+        // Move m − 1 disks from the source to the spare peg, by the same general solving procedure. Rules are not violated, by assumption. This leaves the disk m as a top disk on the source peg.
+        // Move the disk m from the source to the target peg, which is guaranteed to be a valid move, by the assumptions — a simple step.
+        // Move the m − 1 disks that we have just placed on the spare, from the spare to the target peg by the same general solving procedure, so they are placed on top of the disk m without violating the rules.
+        // The base case is to move 0 disks (in steps 1 and 3), that is, do nothing – which obviously doesn't violate the rules.
+        // The full Tower of Hanoi solution then consists of moving n disks from the source peg A to the target peg C, using B as the spare peg.
+        
 /*----- app's state (variables) -----*/
 // game win condition
 let gameFinished;
